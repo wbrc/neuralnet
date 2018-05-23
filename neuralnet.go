7b44d5f464004f7a247d44503d37d1bf7558d1cc
Clemens Weber <clemens@maxime.weber>
2018-05-23 18:26:48 +0200

    add biases to neurons, revised dump and load

--- neuralnet.go
+++ neuralnet.go
@@ -1,4 +1,4 @@
-// Package neuralnet provides basic feed-forward
+// Package neuralnet provides basic feed-forward mlp
 // neural-networks
 package neuralnet
 
@@ -19,38 +19,52 @@ func init() {
 }
 
 type neuron struct {
-	Weights    []float64 // the weights of the inputs
+	weights    []float64 // the weights of the inputs
+	bias       float64   // the bias for the neuron
 	output     float64   // the sum of the weighted inputs
 	activation float64   // the output with applied activation function
 	delta      float64   // delta of the neuron
 }
 
 type layer struct {
-	Neurons []neuron
+	neurons []neuron
 }
 
-// NeuralNetwork represents the neural network as layers and neurons
+// NeuralNetwork that contains layers, neurons and biases. It provides
+// a fully connected network for supervised learning
 type NeuralNetwork struct {
-	Layers []layer
+	layers []layer
 	a      func(float64) float64 // activation function
 	da     func(float64) float64 // derivation of activation function
 }
 
-func sigmoid(z float64) float64 {
-	return 1.0 / (1.0 + math.Exp(-z))
+type networkDump struct {
+	Layers []layerDump
 }
 
-func sigmoidD(z float64) float64 {
-	return sigmoid(z) * (1 - sigmoid(z))
+type layerDump struct {
+	Neurons []neuronDump
+}
+type neuronDump struct {
+	Weights []float64
+	Bias    float64
+}
+
+func sigmoid(x float64) float64 {
+	return 1.0 / (1.0 + math.Exp(-x))
+}
+
+func sigmoidD(x float64) float64 {
+	return sigmoid(x) * (1 - sigmoid(x))
 }
 
 func newLayer(synapses, neurons uint) layer {
 	l := layer{}
 
-	l.Neurons = make([]neuron, 0, neurons)
+	l.neurons = make([]neuron, 0, neurons)
 
 	for i := 0; i < int(neurons); i++ {
-		l.Neurons = append(l.Neurons, newNeuron(synapses))
+		l.neurons = append(l.neurons, newNeuron(synapses))
 	}
 
 	return l
@@ -59,10 +73,11 @@ func newLayer(synapses, neurons uint) layer {
 func newNeuron(synapses uint) neuron {
 	n := neuron{}
 
-	n.Weights = make([]float64, synapses, synapses)
-	for i := range n.Weights {
-		n.Weights[i] = R.NormFloat64()
+	n.weights = make([]float64, synapses, synapses)
+	for i := range n.weights {
+		n.weights[i] = R.NormFloat64()
 	}
+	n.bias = R.NormFloat64()
 
 	return n
 }
@@ -71,18 +86,19 @@ func (n *NeuralNetwork) feedforward(input []float64) {
 
 	// feed input data into input layer
 	for i := range input {
-		n.Layers[0].Neurons[i].activation = input[i]
+		n.layers[0].neurons[i].activation = input[i]
 	}
 
 	// feed forward through each layer
-	for layerIndex := 1; layerIndex < len(n.Layers); layerIndex++ {
-		for neuronIndex := range n.Layers[layerIndex].Neurons {
+	for layerIndex := 1; layerIndex < len(n.layers); layerIndex++ {
+		for neuronIndex := range n.layers[layerIndex].neurons {
 			var output float64
-			for weightIndex := range n.Layers[layerIndex].Neurons[neuronIndex].Weights {
-				output += n.Layers[layerIndex-1].Neurons[weightIndex].activation * n.Layers[layerIndex].Neurons[neuronIndex].Weights[weightIndex]
+			for weightIndex := range n.layers[layerIndex].neurons[neuronIndex].weights {
+				output += n.layers[layerIndex-1].neurons[weightIndex].activation * n.layers[layerIndex].neurons[neuronIndex].weights[weightIndex]
 			}
-			n.Layers[layerIndex].Neurons[neuronIndex].output = output
-			n.Layers[layerIndex].Neurons[neuronIndex].activation = n.a(output)
+			output += n.layers[layerIndex].neurons[neuronIndex].bias
+			n.layers[layerIndex].neurons[neuronIndex].output = output
+			n.layers[layerIndex].neurons[neuronIndex].activation = n.a(output)
 		}
 	}
 }
@@ -90,28 +106,29 @@ func (n *NeuralNetwork) feedforward(input []float64) {
 func (n *NeuralNetwork) backpropagate(targetOutput []float64, eta float64) {
 
 	// calc deltas of output layer
-	for i := range n.Layers[len(n.Layers)-1].Neurons {
-		n.Layers[len(n.Layers)-1].Neurons[i].delta = (targetOutput[i] - n.Layers[len(n.Layers)-1].Neurons[i].activation) * n.da(n.Layers[len(n.Layers)-1].Neurons[i].output)
+	for i := range n.layers[len(n.layers)-1].neurons {
+		n.layers[len(n.layers)-1].neurons[i].delta = (targetOutput[i] - n.layers[len(n.layers)-1].neurons[i].activation) * n.da(n.layers[len(n.layers)-1].neurons[i].output)
 	}
 
 	// calc deltas for hidden layers
-	for i := len(n.Layers) - 2; i > 0; i-- {
-		for j := range n.Layers[i].Neurons {
+	for i := len(n.layers) - 2; i > 0; i-- {
+		for j := range n.layers[i].neurons {
 			var d float64
-			for k := 0; k < len(n.Layers[i+1].Neurons); k++ {
-				d += n.Layers[i+1].Neurons[k].delta * n.Layers[i+1].Neurons[k].Weights[j]
+			for k := 0; k < len(n.layers[i+1].neurons); k++ {
+				d += n.layers[i+1].neurons[k].delta * n.layers[i+1].neurons[k].weights[j]
 			}
 
-			n.Layers[i].Neurons[j].delta = d * n.da(n.Layers[i].Neurons[j].output)
+			n.layers[i].neurons[j].delta = d * n.da(n.layers[i].neurons[j].output)
 		}
 	}
 
-	// update weights
-	for layerIndex := 1; layerIndex < len(n.Layers); layerIndex++ {
-		for neuronIndex := range n.Layers[layerIndex].Neurons {
-			for weightIndex := range n.Layers[layerIndex].Neurons[neuronIndex].Weights {
-				n.Layers[layerIndex].Neurons[neuronIndex].Weights[weightIndex] += eta * n.Layers[layerIndex].Neurons[neuronIndex].delta * n.Layers[layerIndex-1].Neurons[weightIndex].activation
+	// update weights and biases
+	for layerIndex := 1; layerIndex < len(n.layers); layerIndex++ {
+		for neuronIndex := range n.layers[layerIndex].neurons {
+			for weightIndex := range n.layers[layerIndex].neurons[neuronIndex].weights {
+				n.layers[layerIndex].neurons[neuronIndex].weights[weightIndex] += eta * n.layers[layerIndex].neurons[neuronIndex].delta * n.layers[layerIndex-1].neurons[weightIndex].activation
 			}
+			n.layers[layerIndex].neurons[neuronIndex].bias += eta * n.layers[layerIndex].neurons[neuronIndex].delta
 		}
 	}
 
@@ -128,7 +145,7 @@ func NewNeuralNetwork(layers []uint) *NeuralNetwork {
 
 	n := NeuralNetwork{}
 
-	n.Layers = make([]layer, 0, len(layers))
+	n.layers = make([]layer, 0, len(layers))
 
 	for i, layer := range layers {
 		var synapses uint
@@ -139,35 +156,47 @@ func NewNeuralNetwork(layers []uint) *NeuralNetwork {
 			synapses = layers[i-1]
 		}
 
-		n.Layers = append(n.Layers, newLayer(synapses, layer))
+		n.layers = append(n.layers, newLayer(synapses, layer))
 	}
 	n.a = sigmoid
 	n.da = sigmoidD
-
 	return &n
 }
 
 // LoadNeuralNetwork returns a previously dumped network
-// based on the encoded data from r
+// based on the encoded data from the io.Reader r
 func LoadNeuralNetwork(r io.Reader) (*NeuralNetwork, error) {
 	b, err := ioutil.ReadAll(r)
 	if err != nil {
 		return nil, err
 	}
+	var dn networkDump
+	err = json.Unmarshal(b, &dn)
+	if err != nil {
+		return nil, err
+	}
+
 	var n NeuralNetwork
-	n.da = sigmoidD
+	n.layers = make([]layer, len(dn.Layers), len(dn.Layers))
+	for i, l := range dn.Layers {
+		n.layers[i].neurons = make([]neuron, len(l.Neurons), len(l.Neurons))
+		for j, cn := range l.Neurons {
+			n.layers[i].neurons[j] = neuron{cn.Weights, cn.Bias, 0.0, 0.0, 0.0}
+		}
+	}
+
 	n.a = sigmoid
-	err = json.Unmarshal(b, &n)
+	n.da = sigmoidD
 	return &n, err
 }
 
 // Train will feedforward the input and then backpropagate to reduce
 // error on output using learning rate eta
 func (n *NeuralNetwork) Train(input, output []float64, eta float64) {
-	if len(input) != len(n.Layers[0].Neurons) {
+	if len(input) != len(n.layers[0].neurons) {
 		panic("invalid input length")
 	}
-	if len(output) != len(n.Layers[len(n.Layers)-1].Neurons) {
+	if len(output) != len(n.layers[len(n.layers)-1].neurons) {
 		panic("invalid output length")
 	}
 	if eta == 0.0 {
@@ -181,24 +210,34 @@ func (n *NeuralNetwork) Train(input, output []float64, eta float64) {
 // Predict will feedforward the input and then return the prediction
 func (n *NeuralNetwork) Predict(input []float64) []float64 {
 
-	if len(input) != len(n.Layers[0].Neurons) {
+	if len(input) != len(n.layers[0].neurons) {
 		panic("invalid input length")
 	}
 
 	n.feedforward(input)
 
-	output := make([]float64, 0, len(n.Layers[len(n.Layers)-1].Neurons))
+	output := make([]float64, 0, len(n.layers[len(n.layers)-1].neurons))
 
-	for _, e := range n.Layers[len(n.Layers)-1].Neurons {
+	for _, e := range n.layers[len(n.layers)-1].neurons {
 		output = append(output, e.activation)
 	}
 	return output
 }
 
 // Dump marshals the neural network to json and writes it
-// to the specified writer w
+// to the specified io.Writer w
 func (n *NeuralNetwork) Dump(w io.Writer) error {
-	b, err := json.Marshal(n)
+
+	nd := &networkDump{}
+	nd.Layers = make([]layerDump, len(n.layers), len(n.layers))
+	for i := 0; i < len(nd.Layers); i++ {
+		nd.Layers[i].Neurons = make([]neuronDump, len(n.layers[i].neurons), len(n.layers[i].neurons))
+		for j, neuron := range n.layers[i].neurons {
+			nd.Layers[i].Neurons[j] = neuronDump{neuron.weights, neuron.bias}
+		}
+	}
+
+	b, err := json.Marshal(nd)
 	if err != nil {
 		return err
 	}
